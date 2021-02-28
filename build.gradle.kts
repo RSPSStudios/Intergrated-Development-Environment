5d9b0a0d8dc7e3b1ac877ac6e53a3ed0993598cc
David <davidschlachter96@gmail.com>
2021-02-27 23:41:04 -0800

    initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.31" apply false
+}
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+
+    group = "com.javatar"
+    version = "1.0-SNAPSHOT"
+
+    apply(plugin = "kotlin")
+
+    dependencies {
+        "testImplementation"(kotlin("test-junit5"))
+        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.6.0")
+        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile>() {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
